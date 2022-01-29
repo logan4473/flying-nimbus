80a0ece240695212d6b85937f9371126124bbf22
Suryansh Singh Shishodia <29953626+suryansh011@users.noreply.github.com>
2022-01-29 19:51:19 +0530

    clean up

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const port = process.env.PORT || 4000 ;
 const Pool = require('pg').Pool;
 const cors = require('cors');
 const isProduction = process.env.NODE_ENV === "production";
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`;
 const pool = new Pool({
-    connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+      },
   });
-  module.exports = pool;
+  
 const app = express();
 app.use(cors());
 
